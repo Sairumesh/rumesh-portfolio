74eaf190eb7f42ba1b7e3959611574dc20e83a9c
Rumesh <sairumesh123@gmail.com>
2025-11-16 20:07:10 +0800

    Switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { HashRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import { Header } from "@/components/Header";
 import { ParticleBackground } from "@/components/ParticleBackground";
@@ -162,7 +162,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/rumesh-portfolio">
+      <HashRouter>
         <div className="relative min-h-screen">
           <ParticleBackground />
           <Header />
@@ -170,7 +170,7 @@ const App = () => (
             <AnimatedRoutes />
           </main>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
